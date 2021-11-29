cb7d899eb7896f32436cf425637809bfa5187805
Vu Nguyen <nguyentranvu0104@gmail.com>
2021-11-29 13:14:18 -0500

    Added football-app.py

--- football-app.py
+++ football-app.py
@@ -5,34 +5,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-st.title("NBA Player Stats Explorer")
+st.title('NFL Football Stats (Rushing) Explorer')
 
 st.markdown("""
-
-This app performs simple webscraping of NBA player stats data!
-* **Python libraries:** base64, pandas, streamlit, seaborn, and numpy
-* **Data source:** [basketball-reference.com](https://www.basketball-reference.com/).
-
+This app performs simple webscraping of NFL Football player stats data (focusing on Rushing)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
 """)
 
 st.sidebar.header('User Input Features')
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2021))))
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990,2021))))
 
-# Web scraping of NBA player stats
+# Web scraping of NFL player stats
+# https://www.pro-football-reference.com/years/2019/rushing.htm
 @st.cache(suppress_st_warning=True)
 def load_data(year):
-    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
-    html = pd.read_html(url, header = 0)
-
-    # select the only element in the list => print out html for clarification
+    url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
+    html = pd.read_html(url, header = 1)
     df = html[0]
     raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-    
-    # In order to make this line works => streamlit => config.py => line 344 => "arrow" to "legacy"
     raw = raw.fillna(0)
     playerstats = raw.drop(['Rk'], axis=1)
     return playerstats
-
 playerstats = load_data(selected_year)
 
 # Sidebar - Team selection
@@ -40,10 +34,10 @@ sorted_unique_team = sorted(playerstats.Tm.unique())
 selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
 
 # Sidebar - Position selection
-unique_pos = ['C','PF','SF','PG','SG']
+unique_pos = ['RB','QB','WR','FB','TE']
 selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 
-# Filtering data through column Tm and Pos
+# Filtering data
 df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
 st.header('Display Player Stats of Selected Team(s)')
@@ -69,10 +63,7 @@ if st.button('Intercorrelation Heatmap'):
     corr = df.corr()
     mask = np.zeros_like(corr)
     mask[np.triu_indices_from(mask)] = True
-
     with sns.axes_style("white"):
         f, ax = plt.subplots(figsize=(7, 5))
         ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-    
-    # deprecation.showPyplotGlobalUse in streamlit => config.py => line 741 => default_val=False
     st.pyplot()
